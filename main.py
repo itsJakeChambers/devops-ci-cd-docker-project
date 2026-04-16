a169fbc2cad0c1814a527224584b3437b66a7523
itsJakeChambers <kjeanmarie01@gmail.com>
2026-04-16 19:15:29 +0200

    add gitignore and POST method in main.py

--- main.py
+++ main.py
@@ -19,3 +19,16 @@ async def get_lieux():
 
     # renvoyer nos données et code 200 (OK)
     return {'donnees': donnees}, 200
+
+@app.post("/lieux")
+async def post_lieux(lieu: str):
+    # si le lieu est déjà présent, nous ne l'ajoutons pas aux données
+    if lieu in donnees['lieux']:
+        # donc retourner simplement la réponse avec un message disant qu'il existe déjà
+        return {'donnees' : donnees, 'message' : "l'emplacement existe déjà"}
+    
+    # par contre s'il n'est pas présent, nous ajoutons le lieu aux données
+    else:
+        donnees['lieux'].append(lieu)
+        #reponse de retour
+        return {'donnees': donnees, 'message': "l'emplacement a été ajouté"}
